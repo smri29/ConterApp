49c5a0e645a272c09e8fabb939cd81e9580ce80d
smri29 <22203174@iubat.edu>
2024-09-20 19:14:56 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/i29/statefullwidgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.i29.statefullwidgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo',
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  State<Homepage> createState(){
+    return _HomepageState();
+  }
+}
+
+class _HomepageState extends State<Homepage> {
+
+  int counter=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white54,
+      appBar: AppBar(
+        title: Text('Counter'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Center(
+        child: Text('Counter $counter'),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: (){
+              counter--;
+              print(counter);
+              setState(() {
+              });
+            },
+            child: Icon(Icons.remove,color: Colors.black,),
+          ),
+          SizedBox(width: 16,),
+          FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: (){
+              counter++;
+              print(counter);
+              setState(() {
+              });
+            },
+            child: Icon(Icons.add,color: Colors.black,),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/*class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  int counter =0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text('Counter $counter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey,
+          onPressed: (){
+            counter++;
+            print(counter);
+          },
+          child: Icon(Icons.add,color: Colors.green,),
+          ),
+    );
+  }
+}*/
